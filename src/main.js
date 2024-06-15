f46abe5ea29a2cbabff287756a881d00146ed6b5
cel3ntato <adriano2024celentano@gmail.com>
2024-06-15 17:17:03 +0200

    bg_mousemove_test

--- src/main.js
+++ src/main.js
@@ -26,13 +26,20 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
-//
+// start-mousemove-movement
 document.addEventListener('mousemove', function (e) {
-  const container = document.querySelector('.hero-back');
-  const moveX = -(e.clientX / window.innerWidth - 0.5) * 20;
-  const moveY = -(e.clientY / window.innerHeight - 0.5) * 10;
-  container.style.backgroundPosition = `calc(50% + ${moveX}px) calc(50% + ${moveY}px)`;
-  // console.log(
-  //   `Background position: calc(50% + ${moveX}px) calc(50% + ${moveY}px)`
-  // );
+  const containers = document.querySelectorAll('.hero-back, .decor-bg');
+  containers.forEach(container => {
+    const moveX = -(e.clientX / window.innerWidth - 0.5) * 20;
+    const moveY = -(e.clientY / window.innerHeight - 0.5) * 10;
+    container.style.backgroundPosition = `calc(50% + ${moveX}px) calc(50% + ${moveY}px)`;
+    // console.log(`Background position: calc(50% + ${moveX}px) calc(50% + ${moveY}px)`);
+  });
+
+  const contentImages = document.querySelectorAll('.benefits-decor-img');
+  contentImages.forEach(image => {
+    const moveX = -(e.clientX / window.innerWidth - 0.5) * 20;
+    const moveY = -(e.clientY / window.innerHeight - 0.5) * 20;
+    image.style.transform = `translate(${moveX}px, ${moveY}px)`;
+  });
 });
