06ab58200c925eacb52a8441f84d10a7b500008d
cel3ntato <adriano2024celentano@gmail.com>
2024-06-15 16:36:11 +0200

    upd_svg_sprite_and_bg_fx

--- src/main.js
+++ src/main.js
@@ -25,3 +25,14 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     smoothScroll(target);
   });
 });
+
+//
+document.addEventListener('mousemove', function (e) {
+  const container = document.querySelector('.hero-back');
+  const moveX = -(e.clientX / window.innerWidth - 0.5) * 20;
+  const moveY = -(e.clientY / window.innerHeight - 0.5) * 10;
+  container.style.backgroundPosition = `calc(50% + ${moveX}px) calc(50% + ${moveY}px)`;
+  // console.log(
+  //   `Background position: calc(50% + ${moveX}px) calc(50% + ${moveY}px)`
+  // );
+});
