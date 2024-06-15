c461e6af7f3434a2b72114a7172918c91a2a48bd
maksymPr424 <m.prykhodko424@gmail.com>
2024-06-16 01:12:18 +0200

    Add scroll button

--- src/js/header.js
+++ src/js/header.js
@@ -2,17 +2,24 @@ const burgerOpen = document.querySelector('.switch-open-burger');
 const burgerClose = document.querySelector('.switch-close-burger');
 const navList = document.querySelector('.nav-list');
 const nav = document.querySelector('.header-nav');
+const body = document.body;
+const upLink = document.querySelector('.up-link');
 
 const toggleImage = e => {
   e.preventDefault();
 
+  upLink.classList.toggle('up-link-active');
+  body.classList.toggle('toggle-scroll');
   burgerClose.classList.toggle('active-btn');
   burgerOpen.classList.toggle('active-btn');
   nav.classList.toggle('active-nav');
 };
 
 const goToSection = e => {
-  if (e.target.getAttribute('class') === 'nav-link') {
+  if (
+    e.target.getAttribute('class') === 'nav-link' &&
+    window.innerWidth < 768
+  ) {
     toggleImage(e);
   }
 };
