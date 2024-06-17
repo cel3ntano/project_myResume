60b9f22e49fdedfe1ea2b4b206f9e75a07204aaa
cel3ntato <adriano2024celentano@gmail.com>
2024-06-17 02:24:28 +0200

    upd_change-theme.js

--- src/js/change-theme.js
+++ src/js/change-theme.js
@@ -2,8 +2,8 @@ const switcher = document.querySelector('.theme-btn');
 const switchThem = document.querySelector('.switch-theme');
 const body = document.body;
 const changeElems = document.querySelectorAll('.change-them');
-const lightThemePicture = document.querySelector('.light-theme-picture');
-const darkThemePicture = document.querySelector('.dark-theme-picture');
+// const lightThemePicture = document.querySelector('.light-theme-picture');
+// const darkThemePicture = document.querySelector('.dark-theme-picture');
 
 const togglePoints = () => {
   changeElems.forEach(item => item.classList.toggle('change-them'));
@@ -17,10 +17,10 @@ const toggleSwitcherClass = () => {
   switchThem.classList.toggle('edit-circle');
 };
 
-const togglePictures = () => {
-  lightThemePicture.classList.toggle('hidden');
-  darkThemePicture.classList.toggle('hidden');
-};
+// const togglePictures = () => {
+//   lightThemePicture.classList.toggle('hidden');
+//   darkThemePicture.classList.toggle('hidden');
+// };
 
 document.addEventListener('DOMContentLoaded', () => {
   const statusThem = localStorage.getItem('statusThem');
@@ -38,7 +38,7 @@ const changeThem = () => {
   toggleSwitcherClass();
   toggleBodyClass();
   togglePoints();
-  togglePictures();
+  // togglePictures();
 
   const statusThem = switchThem.classList.length > 1;
   localStorage.setItem('statusThem', statusThem);
