8bd7cc27dca515d1f09682f17a723a84f1f46c8b
L-y-u-d-m-y-l-a <lyudmyla.k@hotmail.com>
2024-06-17 18:06:48 +0100

    aboutme-update

---
+++ src/js/about.js
@@ -0,0 +1,16 @@
+// const pgf = document.querySelector('.hidden-text');
+// const button = document.querySelector('.learn-more-button');
+
+// document.addEventListener('DOMContentLoaded', e => {
+//   pgf.style.display = 'none';
+// });
+
+// button.addEventListener('click', () => {
+//   if (pgf.style.display === 'none') {
+//     pgf.style.display = 'block';
+//     button.textContent = 'Hide button';
+//   } else if (pgf.style.display === 'block') {
+//     pgf.style.display = 'none';
+//     button.textContent = 'Load more';
+//   }
+// });
